61e9c4714f2a4ddae8955959e093cabcb3e30130
kjune922 <kjune922@naver.com>
2026-08-05 16:40:31 +0900

    SpringBoot 프로젝트 초기환경세팅

---
+++ src/main/java/com/cloudguard/cloudguard/CloudguardApplication.java
@@ -0,0 +1,14 @@
+package com.cloudguard.cloudguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class CloudguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudguard/cloudguard/CloudguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudguard.cloudguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
