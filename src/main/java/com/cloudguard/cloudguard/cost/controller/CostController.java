c2fa7e913bdb0f65f9a3f4f34f73e6d802be13b3
kjune922 <kjune922@naver.com>
2026-08-11 17:00:34 +0900

    [CostController] api/costs로 주소수정

--- src/main/java/com/cloudguard/cloudguard/cost/controller/CostController.java
+++ src/main/java/com/cloudguard/cloudguard/cost/controller/CostController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/budgets")
+@RequestMapping("/api/costs")
 public class CostController {
 
     private final CostService costService;
