803e6c014797ca8ae9acfe80c0877eebf318394a
kjune922 <kjune922@naver.com>
2026-08-13 12:30:56 +0900

    validator을 Monthlybudget내부 private메소드로 로 변경

--- src/main/java/com/cloudguard/cloudguard/budget/domain/MonthlyBudget.java
+++ src/main/java/com/cloudguard/cloudguard/budget/domain/MonthlyBudget.java
@@ -1,7 +1,5 @@
 package com.cloudguard.cloudguard.budget.domain;
 
-import com.cloudguard.cloudguard.validator.MonthlyLimitValidator;
-import com.cloudguard.cloudguard.validator.YearMonthValidator;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 
@@ -9,14 +7,11 @@ public class MonthlyBudget {
 
     private final YearMonth yearMonth;
     private final BigDecimal monthlyLimit;
-    private final YearMonthValidator yearMonthValidator = new YearMonthValidator();
-    private final MonthlyLimitValidator monthlyLimitValidator = new MonthlyLimitValidator();
 
 
     public MonthlyBudget(YearMonth yearMonth, BigDecimal monthlyLimit) {
-        yearMonthValidator.validateYearMonth(yearMonth);
-        monthlyLimitValidator.validateMonthlyLimit(monthlyLimit);
-
+        validateYearMonth(yearMonth);
+        validateMonthlyLimit(monthlyLimit);
         this.yearMonth = yearMonth;
         this.monthlyLimit = monthlyLimit;
     }
@@ -27,5 +22,17 @@ public class MonthlyBudget {
 
     public YearMonth getYearMonth() {
         return yearMonth;
+    }
+
+    private void validateYearMonth(YearMonth yearMonth){
+        if(yearMonth == null){
+            throw new IllegalArgumentException("연월은 필수입니다.");
+        }
+    }
+
+    private void validateMonthlyLimit(BigDecimal monthlyLimit){
+        if(monthlyLimit == null || monthlyLimit.compareTo(BigDecimal.ZERO) <= 0){
+            throw new IllegalArgumentException("월 예산은 0보다 커야합니다.");
+        }
     }
 }
