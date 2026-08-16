d713ed9bb29a8eb81709ab94b2e66d477965989a
kjune922 <kjune922@naver.com>
2026-08-16 16:15:26 +0900

    test: BudgetControllerTest 404, 409 자동화

--- src/test/java/com/cloudguard/cloudguard/budget/controller/BudgetControllerTest.java
+++ src/test/java/com/cloudguard/cloudguard/budget/controller/BudgetControllerTest.java
@@ -9,12 +9,16 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import com.cloudguard.cloudguard.budget.exception.DuplicateMonthlyBudgetException;
+import com.cloudguard.cloudguard.budget.exception.MonthlyBudgetNotFoundException;
 import java.math.BigDecimal;
 import java.time.YearMonth;
 
@@ -71,6 +75,44 @@ class BudgetControllerTest {
 
         verify(budgetService).addMonthlyBudget(
                 yearMonth,
+                monthlyLimit
+        );
+    }
+
+    @Test
+    void 등록되지않은_연월의_상태조회는_404() throws Exception{
+        YearMonth yearMonth = YearMonth.of(2026,10);
+
+        given(budgetService.determineMonthlyStatus(yearMonth))
+                .willThrow(new MonthlyBudgetNotFoundException());
+
+        mockMvc.perform(get("/api/budgets/status")
+                .param("yearMonth","2026-10"))
+                .andExpect(status().isNotFound());
+
+        verify(budgetService).determineMonthlyStatus(yearMonth);
+    }
+
+    @Test
+    void 같은_연월의_예산을_중복등록하면_409() throws Exception {
+        YearMonth yearMonth1 = YearMonth.of(2026,8);
+        BigDecimal monthlyLimit = BigDecimal.valueOf(1000);
+
+        given(budgetService.addMonthlyBudget(yearMonth1,monthlyLimit))
+                .willThrow(new DuplicateMonthlyBudgetException());
+
+        mockMvc.perform(post("/api/budgets/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "yearMonth" : "2026-08",
+                            "monthlyLimit" : 1000
+                        }
+                        """))
+                .andExpect(status().isConflict());
+
+        verify(budgetService).addMonthlyBudget(
+                yearMonth1,
                 monthlyLimit
         );
     }
