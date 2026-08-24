3b70b850af88922777c3b42b0ff6d902d3127619
kjune922 <kjune922@naver.com>
2026-08-24 16:43:54 +0900

    feat: GlobalExceptionHandler 구현, 예산 미등록 404

---
+++ src/main/java/com/cloudguard/cloudguard/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cloudguard.cloudguard.common.exception;
+
+import com.cloudguard.cloudguard.budget.exception.MonthlyBudgetNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice // RestControllerAdvice는 왜 붙이고 어디에 쓰는거지?
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MonthlyBudgetNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleMonthlyBudgetNotFound(
+            MonthlyBudgetNotFoundException exception,
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ErrorResponse response = new ErrorResponse(
+                LocalDateTime.now(),
+                status.value(),
+                "MONHTLY_BUDGET_NOT_FOUND",
+                exception.getMessage(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(response);
+    }
+}
