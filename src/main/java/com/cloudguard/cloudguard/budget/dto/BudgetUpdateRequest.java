51fefcb97879f6437d5ea6f1aa3457c6047b1d4c
kjune922 <kjune922@naver.com>
2026-08-24 17:53:35 +0900

    feat: [DTO] BudgetUpdateRequest에도 monthlyLimit에 NotNull, Positve 어어노테이션 추가

--- src/main/java/com/cloudguard/cloudguard/budget/dto/BudgetUpdateRequest.java
+++ src/main/java/com/cloudguard/cloudguard/budget/dto/BudgetUpdateRequest.java
@@ -1,9 +1,14 @@
 package com.cloudguard.cloudguard.budget.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public class BudgetUpdateRequest {
 
+    @NotNull(message = "월 예산은 필수입니다.")
+    @Positive(message = "월 예산은 0보다 커야 합니다.")
     private BigDecimal monthlyLimit;
 
     public BudgetUpdateRequest (){
