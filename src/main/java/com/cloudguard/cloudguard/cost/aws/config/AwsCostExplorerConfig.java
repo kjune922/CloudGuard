7ca06d6f17be0567d0e5073066471df78fde634b
kjune922 <kjune922@naver.com>
2026-08-26 20:47:41 +0900

    feat: AwsCostExplorerConfig 구현

---
+++ src/main/java/com/cloudguard/cloudguard/cost/aws/config/AwsCostExplorerConfig.java
@@ -0,0 +1,17 @@
+package com.cloudguard.cloudguard.cost.aws.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.costexplorer.CostExplorerClient;
+
+@Configuration
+public class AwsCostExplorerConfig {
+
+    @Bean
+    public CostExplorerClient costExplorerClient() {
+        return CostExplorerClient.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+    }
+}
